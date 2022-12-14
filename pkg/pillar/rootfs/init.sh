6edaa413286fded6b8ebecfa620b906d8630a924
Roman Penyaev <r.peniaev@gmail.com>
2022-12-14 13:34:51 +0100

    FOR LOCAL CLOUD

--- pkg/pillar/rootfs/init.sh
+++ pkg/pillar/rootfs/init.sh
@@ -5,6 +5,10 @@
 # Start with a default content for resolv.conf
 echo 'nameserver 8.8.8.8' > /etc/resolv.conf
 
+echo '192.168.178.100 zedcontrol.local.zededa.net' >> /etc/hosts
+echo '192.168.178.100 zedcloud.local.zededa.net' >> /etc/hosts
+
+
 #Copy pre-defined fscrypt.conf
 cp fscrypt.conf /etc/fscrypt.conf
 
