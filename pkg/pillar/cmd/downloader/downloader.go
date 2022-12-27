4052c3f17e8c93ae24ede421b2329ce3430dee75
Roman Penyaev <r.peniaev@gmail.com>
2022-12-27 22:03:51 +0100

    downloader: start using the 'DatastoreIDList' field
    Lookup for the datastore ID in the whole datastore list,
    and start using the list of datastores for downloading.
    
    Signed-off-by: Roman Penyaev <r.peniaev@gmail.com>

--- pkg/pillar/cmd/downloader/downloader.go
+++ pkg/pillar/cmd/downloader/downloader.go
@@ -208,13 +208,23 @@ func Run(ps *pubsub.PubSub, loggerArg *logrus.Logger, logArg *base.LogObject, ar
 	}
 }
 
+// lookupDatastore() - does lookup for datastore ID and returns true if found
+func lookupDatastore(dsid_arg uuid.UUID, status types.DownloaderStatus) bool {
+	for _, dsid := range status.DatastoreIDList {
+		if dsid == dsid_arg {
+			return true
+		}
+	}
+	return false
+}
+
 // handle the datastore modification
 func checkAndUpdateDownloadableObjects(ctx *downloaderContext, dsID uuid.UUID) {
 	pub := ctx.pubDownloaderStatus
 	items := pub.GetAll()
 	for _, st := range items {
 		status := st.(types.DownloaderStatus)
-		if status.DatastoreID == dsID {
+		if lookupDatastore(dsID, status) {
 			config := lookupDownloaderConfig(ctx, status.Key())
 			if config != nil {
 				log.Noticef("checkAndUpdateDownloadableObjects updating %s due to datastore %s",
@@ -482,10 +492,8 @@ func doDownload(ctx *downloaderContext, config types.DownloaderConfig, status *t
 		return
 	}
 
-	//TODO: will be used the real list of IDS in the following patches
-	dsids := []uuid.UUID{config.DatastoreID}
-
-	dslist, err := prepareDatastoresList(ctx, config, dsids)
+	// Prepare list of datastore contexts and configs
+	dslist, err := prepareDatastoresList(ctx, config, config.DatastoreIDList)
 	if err != nil {
 		errStr := fmt.Sprintf("Retry download in %v: %s failed: %s",
 			retryTime, config.Name, err)
